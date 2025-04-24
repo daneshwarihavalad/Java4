5e998b01b0e651944df8068db07e34183dd47346
daneshwarihavalad <daneshwarisudheerhavalad@gmail.com>
2025-04-24 14:18:56 +0530

    Save

---
+++ Java4.java
@@ -0,0 +1,11 @@
+ class Demo {
+    public static void main(String[] args) {
+        // Initialize numbers
+        int num1 = 10;
+        int num2 = 5;
+
+        // Perform addition and subtraction
+        System.out.println("Addition: " + (num1 + num2));
+        System.out.println("Subtraction: " + (num1 - num2));
+    }
+}
